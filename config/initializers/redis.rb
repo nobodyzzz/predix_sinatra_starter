61acd6e4e13ef55aa2ea1770b027cbbf3ff1acef
Dmitry Savitski <dmitry.savitsky@rightscale.com>
2016-03-24 15:38:05 +0300

    Configure Redis for Predix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,13 @@
 require "yaml"
 
 # Redis Configuration
-unless ENV['RACK_ENV'] == 'test'
+if ENV['VCAP_SERVICES']
+  require 'json'
+  credentials = JSON.parse(ENV['VCAP_SERVICES'])["redis-1"].first["credentials"]
+  REDIS = Redis.new(credentials)
+else
   redis_settings = YAML::load_file("config/redis.yml")
   REDIS = Redis.new(redis_settings[ENV['RACK_ENV']])
 end
+
+REDIS.set('chunky', 'bacon')
